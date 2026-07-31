22bcf01f2503974afed1f806894ad65f4db289d2
zaighamhaider7 <zaighamhaider8@gmail.com>
2026-07-30 21:15:33 -0700

    errorhandling

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,6 +35,7 @@ const register = async (req, res) => {
         }
     }
     catch (error) {
+        console.log(error);
         return res.status(500).json({
             msg: "Internal Server Error",
         });
